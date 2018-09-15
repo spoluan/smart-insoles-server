bf1d9b1ac3fb6abc980892b30b59585838757c66
shiqi <se7en.thee@gmail.com>
2018-09-16 01:55:22 +0800

    Version one

--- server.py
+++ server.py
@@ -1,9 +1,30 @@
-from flask import Flask 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return "Hello"
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
+import SocketServer
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+if __name__ == "__main__":
+    HOST, PORT = "https://mss1407blab.herokuapp.com/", 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
