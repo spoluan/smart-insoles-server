8689cd331b137a2d710e0750e27ef7b28d74e0f9
shiqi <se7en.thee@gmail.com>
2018-09-16 02:37:30 +0800

    version

--- server.py
+++ server.py
@@ -2,8 +2,20 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
+@app.route('/')
+def sessions():
+    return 'Hello.'
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
