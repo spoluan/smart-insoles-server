f3dbe38e789e8316a24da9acb6122019dbfcc985
shiqi <se7en.thee@gmail.com>
2018-09-16 14:45:56 +0800

    Version 1

--- server.py
+++ server.py
@@ -7,10 +7,14 @@ app = Flask(__name__)
 def index():
     return 'Hello' 
 
-@app.route('/requesting', methods=['POST'])
+@app.route('/requesting', methods=['POST', 'GET', 'PUT'])
 def process_request():
-	print('Recieved from client: {}'.format(request.data))
-	return Response('We received something')
+	if request.method == 'GET':
+		return "Get key: %s, val: %s" % (request.args.get('key'), request.args.get('value'))
+	elif request.method == 'POST':
+		return "Post key: %s, val: %s" % (request.form['key'], request.form['val'])
+	else:
+		return "Put method"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
