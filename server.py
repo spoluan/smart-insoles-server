dfe4d7658cdf110c9fd281e14cbc202e959553e0
shiqi <se7en.thee@gmail.com>
2018-09-16 13:26:35 +0800

    Version 1

--- server.py
+++ server.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return 'Hello' 
 
+@app.route('/request', methods=['GET', 'POST'])
+def process_request():
+	if request.methods == 'GET':
+		return 'GET'
+	else:
+		return 'POST: key = %s, val = %s' % (request.args.get('key'), request.args.get('val'))
+    return 'Hello'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
