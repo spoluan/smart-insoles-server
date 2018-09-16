19384a14fa4919f2725d4fc669c2ad7608b337c2
shiqi <se7en.thee@gmail.com>
2018-09-16 18:01:41 +0800

    Version 1

--- app.py
+++ app.py
@@ -8,11 +8,9 @@ def index():
 
 @app.route('/req', methods=['POST'])
 def prreq():
-    input_json = request.get_json(force=True) 
-#    print 'data from client:', input_json
-    dictToReturn = {'answer': 42}
-    return jsonify(input_json)
-#    return "key %s, val %s" % (request.form['key'], request.form['val'])
+    input_json = request.get_json(force=True)  
+    result = {input_json['key']:input_json['val']}
+    return jsonify(result) 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
