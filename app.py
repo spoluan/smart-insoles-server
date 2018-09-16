ff55fa53beb8c407f28c76500ce12ae4e35f2ec1
shiqi <se7en.thee@gmail.com>
2018-09-16 19:31:49 +0800

    Version 1

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
 def prreq():
     input_json = request.get_json(force=True)   
     heel = input_json['heel']
-#    thumb = input_json['thumb']
+    thumb = input_json['thumb']
 #    out_ball = input_json['out_ball']
 #    inner_ball = input_json['inner_ball']
     
@@ -18,7 +18,7 @@ def prreq():
 #    for i in range(len(heel)):
 #        total = total + int(heel[i]) + int(thumb[i] + int(out_ball[i]) + int(inner_ball[i]))
 #     
-    result = {'heel':heel,'thumb':'thumb','out_ball':'out_ball','inner_ball':'inner_ball'}
+    result = {'heel':heel,'thumb':thumb,'out_ball':'out_ball','inner_ball':'inner_ball'}
     return jsonify(result) 
 
 if __name__ == '__main__':
