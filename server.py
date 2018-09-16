80ae3e0e730013915875b2f007332d2c879b9066
shiqi <se7en.thee@gmail.com>
2018-09-16 13:07:53 +0800

    Version 1

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask
- 
+
+app = Flask(__name__) 
+
 @app.route('/')
 def index():
     return 'Hello' 
 
 if __name__ == '__main__':
-    socketio.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
