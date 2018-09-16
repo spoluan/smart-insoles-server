0532594ee50524979b25e6bc57a95f0574f95e8e
shiqi <se7en.thee@gmail.com>
2018-09-16 15:59:21 +0800

    Version 1

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request, json
+from flask import request
 
 app = Flask(__name__) 
 
@@ -9,13 +9,12 @@ def index():
 
 @app.route('/requesting', methods=['POST', 'GET', 'PUT'])
 def process_request():
-	if request.method == 'GET':
-		return "Get key: %s, val: %s" % (request.args.get('key'), request.args.get('value'))
-	elif request.method == 'POST':
-		return "Post => heel : %s, thumb: %s, out_ball: %s, inner_ball %s" % (request.form['heel'], request.form['thumb'], request.form['out_ball'], request.form['inner_ball'])
- 
-	else:
-		return "Put method"
+    if request.method == 'GET':
+        return "Get key: %s, val: %s" % (request.args.get('key'), request.args.get('value'))
+    elif request.method == 'POST':
+        return "Post => heel : %s, thumb: %s, out_ball: %s, inner_ball %s" % (request.form['heel'], request.form['thumb'], request.form['out_ball'], request.form['inner_ball'])
+    else:
+        return "Put method"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
