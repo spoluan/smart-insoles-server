f959f9166cb4d0bf21d2808737b45c4e499410b9
shiqi <se7en.thee@gmail.com>
2018-09-16 17:23:04 +0800

    Version 1

--- app.py
+++ app.py
@@ -6,21 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Hello' 
 
-@app.route('/requesting', methods=['POST', 'GET', 'PUT'])
+@app.route('/request', methods=['POST'])
 def process_request():
-    if request.method == 'GET':
-        return "Get key: %s, val: %s" % (request.args.get('key'), request.args.get('value'))
-    elif request.method == 'POST':
-        # Ref: https://scotch.io/bar-talk/processing-incoming-request-data-in-flask
-        req_data = request.form 
-        heel = req_data['key']
-        thumb = req_data['val'] 
-        return 'heel = {}, thumb = {}' . format(heel, thumb)
-        
-        #data = request.form
-        #return "Post => heel : %s, thumb: %s, out_ball: %s, inner_ball %s" % (data.get('heel'), data.get('thumb'), data.get('outball'), data.get('innerball'))
-    else:
-        return "Put method"
+    return "key %s, val %s" % (request.form['key'], request.form['val'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
