9ab69e60cc50402d6f6cf7a8a3235e1d400e983f
shiqi <se7en.thee@gmail.com>
2018-09-17 22:58:49 +0800

    Version 1

--- serv.py
+++ serv.py
@@ -23,7 +23,8 @@ class Database(db.Model):
     weight = db.Column(db.Integer)
     time = db.Column(db.String(20))
     
-    def __init__(self, status, weight, time):
+    def __init__(self, id, status, weight, time):
+        self.id = id
         self.status = status
         self.weight = weight
         self.time = time
@@ -58,52 +59,55 @@ def prreq():
             status_ = 'skiped_create'
             pass
         
-    # View table
-    try:
-        if input_json['view'] == 'yes':
-            data = Database.query.all()
-            status = []
-            weight = []
-            time = []
-            for i in data:
-                status.append(i.status)
-                weight.append(i.weight)
-                time.append(i.time)
-            status_ = '{}, {}, {}' . format(status, weight, time)
-            
-#            if status[0] != status[1] and time[0] == time[1]:
-#                right = 0
-#                left = 0
-#                if status[0] == 'right':
-#                    right = int(weight[0])
-#                    left = int(weight[1])
-#                    
-#                if status[0] == 'left':
-#                    left = int(weight[0])
-#                    right = int(weight[1])
-#                     
-#                if right > 700 and left > 700 or (right < 100 and left < 100):
-#                    status_ = 'Normal' # Normal
-#                elif left < 600 and right > 700:
-#                    status_ = 'Right' # Right
-#                elif left > 700 and right < 600:
-#                    status_ = 'Left' # Left
-#                else:
-#                    status_ = 'Normal' # Normal 
-    except: 
-        status_ = 'skipped_view'
-        pass 
+            # View table
+            try:
+                if input_json['view'] == 'yes':
+                    data = Database.query.all()
+                    status = []
+                    weight = []
+                    time = []
+                    for i in data:
+                        status.append(i.status)
+                        weight.append(i.weight)
+                        time.append(i.time)
+                    status_ = '{}, {}, {}' . format(status, weight, time)
+                    
+                    if status[0] != status[1] and time[0] == time[1]:
+                        right = 0
+                        left = 0
+                        if status[0] == 'right':
+                            right = int(weight[0])
+                            left = int(weight[1])
+                            
+                        if status[0] == 'left':
+                            left = int(weight[0])
+                            right = int(weight[1])
+                             
+                        if right > 700 and left > 700 or (right < 100 and left < 100):
+                            status_ = 'Normal' # Normal
+                        elif left < 600 and right > 700:
+                            status_ = 'Right' # Right
+                        elif left > 700 and right < 600:
+                            status_ = 'Left' # Left
+                        else:
+                            status_ = 'Normal' # Normal 
+            except: 
+                status_ = 'skipped_view'
+                pass 
     
-#    # Delete table
-#    try:
-#        if input_json['delete'] == 'yes':
-#            data = Database.query.filter_by(time='1:3:32').first()
-#            db.session.delete(data)
-#            db.session.commit()
-#            status_ = input_json
-#    except:
-#        status_ = 'skipped_delete'
-#        pass
+                # Delete table
+                try:
+                    if input_json['delete'] == 'yes':
+                        data = Database.query.all()
+                        for i in data: 
+                            data = Database.query.filter_by(id=i.id).first()
+                            db.session.delete(data)
+                        
+                        db.session.commit()
+                        status_ = input_json
+                except:
+                    status_ = 'skipped_delete'
+                    pass
       
     passing = {'all_joint': '{}' . format(status_)}
  
