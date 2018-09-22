9a6609638b0b0aaf1a9afed3402465fb4097d110
shiqi <se7en.thee@gmail.com>
2018-09-22 22:05:12 +0800

    Version 1

--- serv.py
+++ serv.py
@@ -29,159 +29,130 @@ def index():
     return 'Hello'  
 
 @app.route('/req', methods=['POST'])
-def prreq(): 
-      
-    status_ = {'':''}
-    
+def prreq():  
+    status_ = {'':''} 
     input_json = request.get_json(force=True)
+    method_status = input_json['METHOD']
     
     # Create table
-    try:  
-        if input_json['create'] == 'yes':
-            db.create_all()
-            status_ = input_json
-    except: 
-        try: # Input data
-            db.session.add(Database(input_json['status'], input_json['weight'], input_json['time']))
-            db.session.commit()
-            try: # View data
-                data = Database.query.all()
-                id = []
-                status = []
-                weight = []
-                time = []
-                for i in data:
-                    id.append(i.id)
-                    status.append(i.status)
-                    weight.append(i.weight)
-                    time.append(i.time)
-                    
-                status_ = '{}, {}, {}, {}, {}' . format(id, status, weight, time, len(data))
-                
-                if len(data) == 2:
-                    try: # Handle array exception
-                        if status[0] != status[1] and time[0] == time[1]:
-                            right = 0
-                            left = 0
-                            if status[0] == 'right':
-                                right = int(weight[0])
-                                left = int(weight[1])
-                                
-                            if status[0] == 'left':
-                                left = int(weight[0])
-                                right = int(weight[1])
-                                 
-                            if right > 700 and left > 700 or (right < 100 and left < 100):
-                                status_ = 'Normal' # Normal
-                            elif left < 600 and right > 700:
-                                status_ = 'Right' # Right
-                            elif left > 700 and right < 600:
-                                status_ = 'Left' # Left
-                            else:
-                                status_ = 'Normal' # Normal
-                            
-                            # Delete table
-                            try: 
-                                data = Database.query.all()
-                                for i in data: 
-                                    data = Database.query.filter_by(id=i.id).first()
-                                    db.session.delete(data)
-                                
-                                db.session.commit() 
-                            except: 
-                                pass
-                        else:
-                            # Delete table
-                            try: 
-                                data = Database.query.all()
-                                for i in data: 
-                                    data = Database.query.filter_by(id=i.id).first()
-                                    db.session.delete(data)
-                                
-                                db.session.commit() 
-                            except: 
-                                pass 
-                            status_ = 'Different'
-                    except:
-                        pass
-            except: 
-                status_ = 'skipped_view'
-                pass 
-        except:     
-            status_ = 'skiped_create'
-            pass
-        
-            # View table
-            try:
-                if input_json['view'] == 'yes':
-                    data = Database.query.all()
-                    id = []
-                    status = []
-                    weight = []
-                    time = []
-                    for i in data:
-                        id.append(i.id)
-                        status.append(i.status)
-                        weight.append(i.weight)
-                        time.append(i.time)
-                        
-                    status_ = '{}, {}, {}, {}, {}' . format(id, status, weight, time, len(data))
-                    
-                    if len(data) == 2:
-                        try: # Handle array exception
-                            if status[0] != status[1] and time[0] == time[1]:
-                                right = 0
-                                left = 0
-                                if status[0] == 'right':
-                                    right = int(weight[0])
-                                    left = int(weight[1])
-                                    
-                                if status[0] == 'left':
-                                    left = int(weight[0])
-                                    right = int(weight[1])
-                                     
-                                if right > 700 and left > 700 or (right < 100 and left < 100):
-                                    status_ = 'Normal' # Normal
-                                elif left < 600 and right > 700:
-                                    status_ = 'Right' # Right
-                                elif left > 700 and right < 600:
-                                    status_ = 'Left' # Left
-                                else:
-                                    status_ = 'Normal' # Normal
-                                
-                                # Delete table
-                                try: 
-                                    data = Database.query.all()
-                                    for i in data: 
-                                        data = Database.query.filter_by(id=i.id).first()
-                                        db.session.delete(data)
-                                    
-                                    db.session.commit() 
-                                except: 
-                                    pass
-                        except:
-                            pass
-            except: 
-                status_ = 'skipped_view'
-                pass 
+    if method_status == 'CREATE':
+        status_ = createTable(input_json)  
     
-                # Delete table
-                try:
-                    if input_json['delete'] == 'yes':
-                        data = Database.query.all()
-                        for i in data: 
-                            data = Database.query.filter_by(id=i.id).first()
-                            db.session.delete(data)
-                        
-                        db.session.commit()
-                        status_ = input_json
-                except:
-                    status_ = 'skipped_delete'
-                    pass
-      
-    passing = {'response': status_}
- 
-    return jsonify(passing) 
+    # Insert data
+    if method_status == 'INSERT': 
+        availability, data_length, id, status, weight, time = getAvailability(input_json)
+        if availability == True: 
+            status_ = insertData(input_json)
+        else:
+            status_ = checkStanding(status, time, weight)
+    
+    # Delete data
+    if method_status == 'DELETE':
+        status_ = deleteData(input_json)
+    
+    # View data
+    if method_status == 'VIEW':
+        data_length, id, status, weight, time = viewData(input_json)
+        status_ = {'STATUS':{'DATA_LENGTH':data_length, 'ID':id, 'STATUS':status, 'WEIGHT': weight, 'TIME':time}}
+       
+    return jsonify(status_) 
+
+def createTable(input_json):
+    try: 
+        db.create_all()
+        status_ = {'STATUS':'CREATE_TABLE_OK'}
+        return status_
+    except:
+        status_ = {'STATUS':'CREATE_TABLE_NO'}
+        return status_
+    
+def insertData(input_json):
+    try:
+        db.session.add(Database(input_json['STATUS'], input_json['WEIGHT'], input_json['TIME']))
+        db.session.commit()
+        status_ = {'STATUS':'INSERT_DATA_OK'}
+        return status_
+    except:
+        status_ = {'STATUS':'INSERT_DATA_NO'}
+        return status_
+            
+def deleteData(input_json):
+    try: 
+        data = Database.query.all()
+        for i in data: 
+            data = Database.query.filter_by(id=i.id).first()
+            db.session.delete(data) 
+        db.session.commit()
+        status_ = {'STATUS':'DELETE_DATA_OK'}
+        return status_
+    except: 
+        status_ = {'STATUS':'DELETE_DATA_NO'}
+        return status_
+
+def viewData(input_json):
+    try: 
+        data = Database.query.all()
+        id = []
+        status = []
+        weight = []
+        time = []
+        for i in data:
+            id.append(i.id)
+            status.append(i.status)
+            weight.append(i.weight)
+            time.append(i.time)  
+        return len(data), id, status, weight, time
+    except:
+        return 'EMPTY'
+    
+def getAvailability(input_json):
+    try: 
+        data_length, id, status, weight, time = viewData(input_json)
+        
+        if data_length == 2:
+            if (status[0] == 'RIGHT' and status[1] == 'LEFT') or (status[0] == 'LEFT' and status[1] == 'RIGHT'): 
+                return False, id, status, weight, time
+        if data_length == 1:
+            return True 
+    except: 
+        return False
+    
+def checkStanding(status, time, weight): 
+    try: # Handle array exception   
+        if status[0] != status[1] and time[0] == time[1]:
+            right = 0
+            left = 0
+            if status[0] == 'RIGHT' and status[1] == 'LEFT':
+                right = int(weight[0])
+                left = int(weight[1])
+                
+                if right > 700 and left > 700 or (right < 100 and left < 100):
+                    status_ = 'NORMAL' # Normal
+                elif left < 600 and right > 700:
+                    status_ = 'RIGHT' # Right
+                elif left > 700 and right < 600:
+                    status_ = 'LEFT' # Left
+                else:
+                    status_ = 'NORMAL' # Normal 
+                    
+            if status[0] == 'LEFT' and status[1] == 'RIGHT':
+                left = int(weight[0])
+                right = int(weight[1])
+                
+                if right > 700 and left > 700 or (right < 100 and left < 100):
+                    status_ = 'NORMAL' # Normal
+                elif left < 600 and right > 700:
+                    status_ = 'RIGHT' # Right
+                elif left > 700 and right < 600:
+                    status_ = 'LEFT' # Left
+                else:
+                    status_ = 'NORMAL' # Normal 
+                  
+        return {'STATUS':status_}
+    except:
+        status_ = {'STATUS':'CHECKING_ERROR'}
+        return status_
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
