493bf1018b7accf663ef6252e61e9e434748989c
spoluan <sevendipoluan@gmail.com>
2022-03-05 20:21:52 +0800

    debugging cant load plugin sqlalchemy issues

--- serv.py
+++ serv.py
@@ -14,7 +14,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
   
 app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']   
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'].replace("postgres://", "postgresql://", 1)
 db = SQLAlchemy(app)
 
 print('Database url:', os.environ['DATABASE_URL'])
